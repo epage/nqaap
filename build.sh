cb03af9172d854612930bcbf404cfd42414365cc
Ed Page <eopage@byu.net>
2010-12-18 16:14:45 -0600

    Cleaning all of this up after the conversion

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-rm -r ~/nqaap;
-svn export http://www.pengworld.com/python/n900/Audiobook/nqaap/ ~/nqaap
-chmod +x ~/nqaap/src/opt/Nqa-Audiobook-player/nqaap.py
-python ~/nqaap/build_nqaap.py
-
