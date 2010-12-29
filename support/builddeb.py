900e6107d5111ccba40c38e1a219cf0d4374545d
Ed Page <eopage@byu.net>
2010-12-29 07:42:49 -0600

    Bump to 0.8.7

--- support/builddeb.py
+++ support/builddeb.py
@@ -25,9 +25,7 @@ def build_package(distribution):
 	except:
 		pass
 
-	p=py2deb.Py2deb("nqaap") #This is the package name and MUST be in
-							   #lowercase! (using e.g. "mClock" fails
-							   #miserably...)
+	p=py2deb.Py2deb("nqaap")
 	p.prettyName="NQA Audiobook Player"
 	p.description="""Very simple Audiobook player.
 Supports playing, pausing, seeking (sort of) and saving state when changing book/closing.
@@ -56,40 +54,25 @@ Homepage: http://wiki.maemo.org/Nqaap"""
 		"diablo": "user/multimedia",
 		"fremantle": "user/multimedia",
 	}[distribution]
-	p.icon = "src/usr/share/icons/hicolor/48x48/hildon/nqaap.png"
-	p.arch="all"				#should be all for python, any for all arch
-	p.urgency="low"			 #not used in maemo onl for deb os
+	p.icon = {
+		"debian": "src/usr/share/icons/hicolor/26x26/hildon/nqaap.png",
+		"diablo": "src/usr/share/icons/hicolor/26x26/hildon/nqaap.png",
+		"fremantle": "src/usr/share/icons/hicolor/48x48/hildon/nqaap.png",
+	}[distribution]
+	p.arch="all"
+	p.urgency="low"
 	p.distribution=distribution
 	p.repository="extras"
 	p.bugTracker="https://bugs.maemo.org/enter_bug.cgi?product=nQa%%20Audiobook%%20Player"
 	p.postinstall="""#!/bin/sh
 rm -f ~/.nqaap/nqaap.log
 """
-	#  p.postremove="""#!/bin/sh
-	#  chmod +x /usr/bin/mclock.py""" #Set here your post remove script
-	#  p.preinstall="""#!/bin/sh
-	#  chmod +x /usr/bin/mclock.py""" #Set here your pre install script
-	#  p.preremove="""#!/bin/sh
-	#  chmod +x /usr/bin/mclock.py""" #Set here your pre remove script
-	version = "0.8.6"		   #Version of your software, e.g. "1.2.0" or "0.8.2"
-	build = "0" #Build number, e.g. "1" for the first build of this
-								#version of your software. Increment
-								#for later re-builds of the same
-								#version of your software.  Text with
-								#changelog information to be displayed
-								#in the package "Details" tab of the
-								#Maemo Application Manager
+	version = "0.8.7"
+	build = "0"
 	changeloginformation = """
-* About window as requested by magnuslu
-* Switched how storage of active book, chapter, and chapter position is stored
+* Fixing a bug with initially configuring the book location
 """.strip()
-	dir_name = "src" #Name of the subfolder containing your package
-								#source files
-								#(e.g. usr\share\icons\hicolor\scalable\myappicon.svg,
-								#usr\lib\myapp\somelib.py). We suggest
-								#to leave it named src in all projects
-								#and will refer to that in the wiki
-								#article on maemo.org
+	dir_name = "src"
 	#Thanks to DareTheHair from talk.maemo.org for this snippet that
 	#recursively builds the file list
 	for root, dirs, files in os.walk(dir_name):
